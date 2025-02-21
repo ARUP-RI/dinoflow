85d51b482d8a2d18f66a0f326ab3a40229cefebd
O'Fallon, Brendan <brendan.o'fallon@aruplab.com>
2025-02-21 15:03:35 -0700

    Track model dim

--- src/dinoflow/models.py
+++ src/dinoflow/models.py
@@ -8,6 +8,7 @@ class TubeEncoder(nn.Module):
     
     def __init__(self, num_features, model_embed_dim, layers, heads):
         super().__init__()
+        self.model_dim = model_embed_dim
         self.fc = nn.Linear(num_features, model_embed_dim)
         encoderlayer = nn.TransformerEncoderLayer(d_model=model_embed_dim, nhead=heads, batch_first=True, dropout=0)
         self.encoder = nn.TransformerEncoder(encoderlayer, num_layers=layers)
