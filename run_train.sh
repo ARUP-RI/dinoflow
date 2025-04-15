e59911ce2bd8641196adf0235914a224d5694bef
Brendan <22319@MLAGNURIAI03D.aruplab.net>
2025-04-15 08:41:34 -0600

    Add dinoflow training script

---
+++ run_train.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+export CUDA_VISIBLE_DEVICES=4,5,6,7
+
+
+export OMP_NUM_THREADS=8
+
+export TORCH_NCCL_TRACE_BUFFER_SIZE=1 # This is for debugging NCCL issues
+
+
+# Implements DinoFlow to train single tube model
+
+torchrun --standalone --nnodes=1 --nproc-per-node=4  src/dinoflow/dino.py \
+    $1 \
+    --tube-type m \
+    --checkpoint /home/22319/src/dinoflow/m_p4k_cont_lessaug/m_p4k_cont_lessaug_epoch80.pt \
+    --run-name $2 \
