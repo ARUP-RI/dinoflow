213ffc173b0d6068e69d8e072c48e0680b08b959
Brendan <22319@MLAGNURIAI03D.aruplab.net>
2025-06-09 16:35:15 -0600

    Fix merge

--- run_eval3tubes.sh
+++ run_eval3tubes.sh
@@ -10,19 +10,21 @@ export TORCH_NCCL_TRACE_BUFFER_SIZE=1 # This is for debugging NCCL issues
 
 # Produces a single unified model from checkpoints/backbones for each of the three tubes 
 
-python src/dinoflow/eval.py train3tubes \
-    /home/22319/data/brendan/dinoflow/checkpoints/b_p4k_fix_koleo/b_p4k_fix_koleo_epoch99.pt \
-    /home/22319/data/brendan/dinoflow/checkpoints/t_p4k_fix_koleo/t_p4k_fix_koleo_epoch99.pt \
-    /home/22319/data/brendan/dinoflow/checkpoints/m_p4k_fix_koleo/m_p4k_fix_koleo_epoch99.pt \
+uv run src/dinoflow/eval.py train3tubes \
+    $1 \
     /home/22319/data/brendan/dinoflow/casedx_2024-08-21_noreport_train_with_m_projections.csv \
     /home/22319/data/brendan/dinoflow/casedx_2024-08-21_noreport_test_with_m_projections.csv \
-    $1 \
-    --labelkey 'CLL' \
+    /home/22319/data/brendan/dinoflow/checkpoints/b_sml_moreaug/b_sml_moreaug_epoch99.pt \
+    /home/22319/data/brendan/dinoflow/checkpoints/t_sml_moreaug/t_sml_moreaug_epoch99.pt \
+    /home/22319/data/brendan/dinoflow/checkpoints/m_sml_moreaug/m_sml_moreaug_epoch99.pt \
+    --labelkey 'NORMAL' \
     --positive-repeat-factor 1 \
     --dataroot /data2/brendan/flow/ \
-    --freeze-backbone-layers 6 \
+    --freeze-backbone-layers 2 \
     --events 4096 \
     --max-lr 0.00025 \
     --batch-size 32 \
-    --epochs 50
+    --epochs 50 \
+    --comet-workspace brendan \
+    --comet-project dinoflow-normal
 
